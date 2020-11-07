450e421ba6fad0fc57ea3cb1182900c653f118a2
Beau Weise <beauweise@gmail.com>
2020-11-07 10:51:03 -0600

    working onclick to details page

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -12,14 +12,17 @@ class Home extends Component {
     getFavorites = () => {
         this.props.dispatch({ type: 'GET_MOVIES' })
     }
+    getDetails = () =>{
+        this.props.history.push('/Details');
+    }
 
     render() {
         return (
-            <div>
+            <div className = 'movies'>
 
                 {/* {JSON.stringify(this.props.reduxState.movies)} */}
                 {this.props.reduxState.movies.map((movie) => {
-                    return <img key={movie.id} alt="" src={movie.poster} />
+                    return <img onClick = {this.getDetails} key={movie.id} alt="" src={movie.poster} />
                 })}
             </div>
         );
